90e6e5f08523f1aa65bfc6e8b5abfa70e0759b70
jakariacst318 <jsjakariacst@gmail.com>
2024-08-17 18:51:04 +0600

    dashboad cart delete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -43,17 +43,27 @@ async function run() {
             res.send(result)
         })
 
-        
+
         // cart collection 
-        app.get('/carts', async (req, res) =>{
-            const result = await cartCollection.find().toArray();
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email }
+            const result = await cartCollection.find(query).toArray();
             res.send(result)
         })
 
         app.post('/carts', async (req, res) => {
-        const cartItem = req.body;
-        const result =  await cartCollection.insertOne(cartItem);
-        res.send(result)
+            const cartItem = req.body;
+            const result = await cartCollection.insertOne(cartItem);
+            res.send(result)
+        })
+
+        // delete
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await cartCollection.deleteOne(query);
+            res.send(result)
         })
 
 
