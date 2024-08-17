03b8c4fbd402b0ef4731f0c3be973eb76efea08d
jakariacst318 <jsjakariacst@gmail.com>
2024-08-17 13:01:58 +0600

    carts collection add

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ async function run() {
 
         const menuCollection = client.db("bistroDB").collection("menu");
         const reviewCollection = client.db("bistroDB").collection("reviews");
+        const cartCollection = client.db("bistroDB").collection("carts");
 
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray()
@@ -40,6 +41,19 @@ async function run() {
         app.get('/reviews', async (req, res) => {
             const result = await reviewCollection.find().toArray()
             res.send(result)
+        })
+
+        
+        // cart collection 
+        app.get('/carts', async (req, res) =>{
+            const result = await cartCollection.find().toArray();
+            res.send(result)
+        })
+
+        app.post('/carts', async (req, res) => {
+        const cartItem = req.body;
+        const result =  await cartCollection.insertOne(cartItem);
+        res.send(result)
         })
 
 
