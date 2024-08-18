9fec3a2611e00b4498a90b800559671206a6d347
jakariacst318 <jsjakariacst@gmail.com>
2024-08-18 13:09:04 +0600

    new user add and next try  user already exists

--- index.js
+++ index.js
@@ -29,9 +29,27 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const userCollection = client.db("bistroDB").collection("users");
         const menuCollection = client.db("bistroDB").collection("menu");
         const reviewCollection = client.db("bistroDB").collection("reviews");
         const cartCollection = client.db("bistroDB").collection("carts");
+
+
+        // user related api
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            //  insert email if user do not exits :
+            //  you cam do this many way (1.email unique, 2.upsert, 3.simple checking)
+            const query = {email: user.email}
+            const existingUser = await userCollection.findOne(query);
+            if(existingUser){
+                return res.send({message: 'user already exists', insertedId: null})
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
 
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray()
