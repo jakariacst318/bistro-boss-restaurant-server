10929b8e0109a42b06ca5dae1be4d9904ef8ba90
jakariacst318 <jsjakariacst@gmail.com>
2024-08-21 00:26:26 +0600

    menu item update

--- index.js
+++ index.js
@@ -141,6 +141,31 @@ async function run() {
             res.send(result)
         })
 
+        // menu item update
+        app.get('/menu/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await menuCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.patch('/menu/:id', async (req, res) => {
+            const item = req.body;
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const updateDoc = {
+                $set: {
+                    name: item.name,
+                    category: item.category,
+                    price: item.price,
+                    recipe: item.recipe,
+                    image: item.image
+                },
+            };
+            const result = await menuCollection.updateOne(filter, updateDoc);
+            res.send(result)
+        })
+
         app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
             const item = req.body;
             const result = await menuCollection.insertOne(item)
